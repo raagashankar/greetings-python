7f8dfbd7934c090e895e67716e552d07a10d4d3e
raagashankar <raaga.shankar@ryerson.ca>
2019-05-10 14:13:37 -0400

    entering greeting

---
+++ greeting.py
@@ -0,0 +1,11 @@
+def greeting():
+    user_in=input("Please enter your name")
+    print ("Hi {}".format(user_in))
+
+def main():
+    greeting()
+
+if __name__ == "__main__":
+    main()
+
+        
